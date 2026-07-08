3ff49b2d344bbf0f175b0ab21c53af5adcbd8a9d
TraiNguyenVan <traipupgmobile@gmail.com>
2026-07-08 17:38:28 +0700

    remove unused libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,12 @@
-#include <iostream>
 #include "library.hpp"
 #include "tui.hpp"
-#include <string>
-#include <deque>
-
-#include "ftxui/component/component.hpp"
-#include "ftxui/component/screen_interactive.hpp"
-#include "ftxui/dom/elements.hpp"
 
 using namespace std;
 
 int main()
 {
-
-    // const string database = "database.txt";
     Library Lib1{1, "Library"};
 
     TUI tui1(Lib1);
-
-    // Lib1.loadFromFile(database);
-    // Lib1.saveToFile(database);
-
     return 0;
 }
