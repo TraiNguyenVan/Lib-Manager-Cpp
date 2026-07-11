a09287967ab52e77f06104f093f4fca7a537fa14
TraiNguyenVan <traipupgmobile@gmail.com>
2026-07-11 23:35:01 +0700

    feat(date): add operator overloading

--- include/date.hpp
+++ include/date.hpp
@@ -1,6 +1,7 @@
 #ifndef DATE_HPP
 #define DATE_HPP
 
+#include <ostream>
 #include <string>
 
 using namespace std;
@@ -13,9 +14,14 @@ private:
     int year;
 
 
-    int monthDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static constexpr int monthDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static long daysInMonth(int month, int year);
+    
+    long toDayCount() const;
+    static Date fromDayCount(long totalDays);
 
-    bool isLeapYear(int year);
+
+    static bool isLeapYear(int year);
     int CalculateLeapYears(int year1, int year2);
     int CalculateDays(Date date);
     //Calculating by transforming the date into days (from 1950 to date calculating date)
@@ -24,6 +30,18 @@ public:
     Date(int day, int month, int year);
     //Calculating how many days have passed since the end of term date
     int daysSinceDeadline(Date endOfTermDate);
+    bool operator==(const Date &other) const;
+    bool operator!=(const Date &other) const;
+    bool operator<(const Date &other) const;
+    bool operator>(const Date &other) const;
+    bool operator<=(const Date &other) const;
+    bool operator>=(const Date &other) const;
+    Date operator+(int daysToAdd) const;
+    Date operator-(int daysToSubtract) const;
+    long operator-(const Date &other) const;
+    friend std::ostream &operator<<(std::ostream &os, const Date &d);
+
+    // bool operator
 };
 
 #endif
--- src/date.cpp
+++ src/date.cpp
@@ -8,6 +8,98 @@ Date::Date(int day, int month, int year)
     this->year = year;
 }
 
+long Date::daysInMonth(int month, int year) {
+    if (month == 2 && isLeapYear(year))
+        return 29;
+    // month - 1 cause array start from 0
+    return monthDays[month - 1];
+}
+long Date::toDayCount() const
+{
+    long total = 0;
+    for (int y = 0; y < year; y++)
+        total += (isLeapYear(y)) ? 366 : 365;
+    for (int m = 1; m < month; m++)
+        total += daysInMonth(m, year);
+    total += day;
+    return total;
+}
+
+Date Date::fromDayCount(long totalDays)
+{
+    int year = 0;
+    while (true) {
+        long daysThisYear = isLeapYear(year) ? 366 : 365;
+        if (totalDays <= daysThisYear) break;
+        totalDays -= daysThisYear;
+        year++;
+    }
+
+    int month = 1;
+    while (true) {
+        long daysThisMonth = daysInMonth(month, year);
+        if (totalDays <= daysThisMonth) break;
+        totalDays -= daysThisMonth;
+        month++;
+    }
+
+    int day = totalDays;
+
+    return Date(day, month, year);
+}
+
+bool Date::operator<(const Date& other) const
+{
+    return toDayCount() < other.toDayCount();
+}
+
+bool Date::operator<=(const Date& other) const
+{
+    return toDayCount() <= other.toDayCount();
+}
+
+bool Date::operator>(const Date& other) const
+{
+    return toDayCount() > other.toDayCount();
+}
+
+bool Date::operator>=(const Date& other) const
+{
+    return toDayCount() >= other.toDayCount();
+}
+
+bool Date::operator==(const Date& other) const
+{
+    return toDayCount() == other.toDayCount();
+}
+
+bool Date::operator!=(const Date& other) const
+{
+    return toDayCount() != other.toDayCount();
+}
+
+Date Date::operator+(int daysToAdd) const
+{
+    return fromDayCount(toDayCount() + daysToAdd);
+}
+
+Date Date::operator-(int daysToSubtract) const
+{
+    return fromDayCount(toDayCount() - daysToSubtract);
+}
+
+long Date::operator-(const Date& other) const
+{
+    return toDayCount() - other.toDayCount();
+}
+std::ostream &operator<<(std::ostream &os, const Date &d)
+{
+    os << (d.day < 10 ? "0" : "") << d.day << "/"
+       << (d.month < 10 ? "0" : "") << d.month << "/"
+       << d.year;
+    return os;
+}
+
 bool Date::isLeapYear(int year)
 {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
@@ -56,4 +148,7 @@ int Date::daysSinceDeadline(Date endOfTermDate)
     int DayPasses = 0;
 
     return CalculateDays(*this) - CalculateDays(endOfTermDate);
+    // with operators overloading implemented, this function can be one-line as below
+    // abstraction absolute over-engineering 
+    return (*this > endOfTermDate) ? (*this - endOfTermDate) : 0;
 }   
