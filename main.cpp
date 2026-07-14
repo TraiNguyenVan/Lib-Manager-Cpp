4ea362663711e94df248a49062f8e3e1a2f7aac5
TraiNguyenVan <traipupgmobile@gmail.com>
2026-07-14 21:03:08 +0700

    style(clang-format): Add clang-format
    I am adding clang-format as default formater for this project, for more info you can check Github issue #22

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
-#include "include/library.hpp"
-#include "include/date.hpp"
-#include "include/student.hpp"
 #include <iostream>
+
+#include "include/date.hpp"
+#include "include/library.hpp"
+#include "include/student.hpp"
 
 using namespace std;
 
-void DateTest(){
+void DateTest() {
     Date d1(1, 1, 2020);
     Date d2(1, 1, 2021);
     cout << "From 1/1/2020 to 1/1/2021 is " << d2.daysSinceDeadline(d1) << " days." << endl;
 }
 
-void StudentInputTest(){
+void StudentInputTest() {
     Library lib(1, "Test Library");
     lib.loadStudents("./data/student-data.txt.example");
-    //cout << "alo" << endl;
-    for(const auto& it : lib.ClassList){
+    // cout << "alo" << endl;
+    for (const auto& it : lib.ClassList) {
         cout << "Class: " << it.first << ", Student IDs: ";
-        for(const auto& studentID : it.second){
+        for (const auto& studentID : it.second) {
             cout << studentID << " ";
         }
         cout << endl;
     }
 }
 
-int main()
-{
+int main() {
     DateTest();
     StudentInputTest();
     return 0;
